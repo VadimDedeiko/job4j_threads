5abe16c66d17243c4580670c2c87745351a82a90
Windows <ivinjc@gmail.com>
2022-07-02 13:32:53 +0300

    Состояние нити. [#229175 #317781]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -4,20 +4,19 @@ public class ThreadState {
     public static void main(String[] args) {
         Thread first = new Thread(
                 () -> System.out.println(Thread.currentThread().getName()));
-        System.out.println(first.getState());
-        first.start();
-        while (first.getState() != Thread.State.TERMINATED) {
-            System.out.println(first.getState());
-        }
-        System.out.println(first.getState() + " Нить 1 завершила работу");
-
         Thread second = new Thread(
                 () -> System.out.println(Thread.currentThread().getName()));
-        System.out.println(second.getState());
+        first.start();
         second.start();
-        while (second.getState() != Thread.State.TERMINATED) {
+        System.out.println(first.getState());
+        System.out.println(second.getState());
+
+        while (first.getState() != Thread.State.TERMINATED
+                && second.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getState());
             System.out.println(second.getState());
         }
+        System.out.println(first.getState() + " Нить 1 завершила работу");
         System.out.println(second.getState() + " Нить 2 завершила работу");
         System.out.println("работа MAIN завершена");
     }
