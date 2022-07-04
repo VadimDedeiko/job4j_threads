6e48ead0e5fb583b22c817d318a4084c95d11f0d
Windows <ivinjc@gmail.com>
2022-07-04 08:05:37 +0300

    Visibility. Общий ресурс вне критической секции [#1102 #318420]

--- src/main/java/ru/job4j/concurrent/ParseFileGet.java
+++ src/main/java/ru/job4j/concurrent/ParseFileGet.java
@@ -1,6 +1,9 @@
 package ru.job4j.concurrent;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.function.Predicate;
 
 public final class ParseFileGet {
@@ -10,16 +13,26 @@ public final class ParseFileGet {
         this.file = file;
     }
 
-    public synchronized String getContent(Predicate<Character> filter) throws IOException {
+    public synchronized String content(Predicate<Character> filter) {
         String output = null;
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
             int data;
-            while ((data = bufferedReader.read()) > -1) {
+            while ((data = bufferedReader.read()) != -1) {
                 if (filter.test((char) data)) {
                     output += (char) data;
                 }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return output;
+    }
+
+    public String getContent() {
+        return content(e -> true);
+    }
+
+    public String getContentWithoutUnicode() {
+        return content(e -> e < 0x80);
     }
 }
