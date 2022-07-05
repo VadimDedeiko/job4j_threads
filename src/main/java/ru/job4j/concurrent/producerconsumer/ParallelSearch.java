062cd6a07316c05e28882519df7e7ba1824e704c
Windows <ivinjc@gmail.com>
2022-07-05 20:56:24 +0300

    Обеспечить остановку потребителя. [#66825]

---
+++ src/main/java/ru/job4j/concurrent/producerconsumer/ParallelSearch.java
@@ -0,0 +1,29 @@
+package ru.job4j.concurrent.producerconsumer;
+
+public class ParallelSearch {
+
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>();
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        System.out.println(queue.poll());
+                    }
+                }
+        );
+        consumer.start();
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                            queue.offer(index);
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                    consumer.interrupt();
+                }
+        ).start();
+    }
+}
