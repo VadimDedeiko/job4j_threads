6ae8244be13132062af242dff413a9710201eab4
Windows <ivinjc@gmail.com>
2022-07-09 11:38:48 +0300

    CAS - операции [#6859 #320081]

--- src/main/java/ru/job4j/concurrent/cas/CASCount.java
+++ src/main/java/ru/job4j/concurrent/cas/CASCount.java
@@ -1,26 +1,21 @@
 package ru.job4j.concurrent.cas;
-
 import net.jcip.annotations.ThreadSafe;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
 @ThreadSafe
 public class CASCount {
-    private final AtomicReference<Integer> count = new AtomicReference<>();
+    private final AtomicInteger count = new AtomicInteger();
 
     public void increment() {
-        Integer incremented = count.get() + 1;
         Integer check;
         do {
             check = count.get();
-        } while (!count.compareAndSet(check, incremented));
+        } while (!count.compareAndSet(check, check + 1));
     }
 
     public int get() {
-        Integer get = count.get();
-        if (get == null) {
-            throw new UnsupportedOperationException("Count is not impl.");
-        }
-        return get;
+        return count.get();
     }
 }
