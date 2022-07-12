82735733dfca588715e6ee70fc10b5130087bcec
Windows <ivinjc@gmail.com>
2022-07-12 19:28:52 +0300

    CAS - операции [#6859 #320081]

--- src/test/java/ru/job4j/concurrent/cas/CASCountTest.java
+++ src/test/java/ru/job4j/concurrent/cas/CASCountTest.java
@@ -3,18 +3,38 @@ package ru.job4j.concurrent.cas;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class CASCountTest {
     @Test
     public void when() throws InterruptedException {
         CASCount casCount = new CASCount();
-        Thread thread1 = new Thread(casCount::increment);
-        Thread thread2 = new Thread(casCount::increment);
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 60; i++) {
+                casCount.increment();
+            }
+        });
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 60; i++) {
+                casCount.increment();
+            }
+        });
+        Thread thread3 = new Thread(() -> {
+            for (int i = 0; i < 60; i++) {
+                casCount.increment();
+            }
+        });
+        Thread thread4 = new Thread(() -> {
+            for (int i = 0; i < 60; i++) {
+                casCount.increment();
+            }
+        });
         thread1.start();
         thread2.start();
         thread2.join();
-        Assert.assertEquals(2, casCount.get());
+        thread3.start();
+        thread3.join();
+        thread4.start();
+        thread4.join();
+        Assert.assertEquals(240, casCount.get());
     }
 
 }
