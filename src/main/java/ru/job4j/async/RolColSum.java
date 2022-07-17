87e005604000d40672a4b4bbe11616ab55663fe9
Windows <ivinjc@gmail.com>
2022-07-17 21:09:05 +0300

    CompletableFuture [#361626 #320697]

--- src/main/java/ru/job4j/async/RolColSum.java
+++ src/main/java/ru/job4j/async/RolColSum.java
@@ -56,34 +56,6 @@ public class RolColSum {
      * 3. Реализовать асинхронную версию программы. i - я задача считает сумму по i столбцу и i строке
      */
     public static Sums[] asyncSum(int[][] matrix) throws ExecutionException, InterruptedException {
-       /* int n = matrix.length;
-        int[] sums = new int[2 * n];
-        Map<Integer, CompletableFuture<Integer>> futures = new HashMap<>();
-        // считаем сумму по главной диагонали
-        futures.put(0, getTask(matrix, 0, n - 1, n - 1));
-        // считаем суммы по побочным диагоналям
-        for (int k = 1; k <= n; k++) {
-            futures.put(k, getTask(matrix, 0, k - 1,  k - 1));
-            if (k < n) {
-                futures.put(2 * n - k, getTask(matrix, n - k, n - 1, n - 1));
-            }
-        }
-        for (Integer key : futures.keySet()) {
-            sums[key] = futures.get(key).get();
-        }
-        return sums;
-    }
-
-    public static CompletableFuture<Integer> getTask(int[][] data, int startRow, int endRow, int startCol) {
-        return CompletableFuture.supplyAsync(() -> {
-            int sum = 0;
-            int col = startCol;
-            for (int i = startRow; i <= endRow; i++) {
-                sum += data[i][col];
-                col--;
-            }
-            return sum;
-        });*/
         Sums[] sums = new Sums[matrix.length];
         Map<Integer, CompletableFuture<Integer[]>> futures = new HashMap<>();
         for (int i = 0; i < matrix.length; i++) {
@@ -113,19 +85,5 @@ public class RolColSum {
             }
             return res;
         });
-    }
-
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        int[][] array = {
-                {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 9}
-        };
-        Sums[] sum = RolColSum.sum(array);
-        System.out.println(sum[0].getColSum());
-        System.out.println(sum[0].getRowSum());
-        Sums[] asyncSum = RolColSum.asyncSum(array);
-        System.out.println(asyncSum[0].getColSum());
-        System.out.println(asyncSum[0].getRowSum());
     }
 }
